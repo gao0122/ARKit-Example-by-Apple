dafd3bff8b7a8cf327c4f697a8b62b43bc184712
Henry Ho <henry.ho@ussuning.com>
2017-08-15 17:06:07 -0700

    Updated to adhere to Beta 5 API

--- ARKitExample/Utilities.swift
+++ ARKitExample/Utilities.swift
@@ -15,7 +15,7 @@ extension UIImage {
         guard let ciImage = CIImage(image: self) else {
             return nil
         }
-        return UIImage(ciImage: ciImage.applyingFilter("CIColorInvert", withInputParameters: nil))
+        return UIImage(ciImage: ciImage.applyingFilter("CIColorInvert", parameters: [:]))
     }
 	
 	static func composeButtonImage(from thumbImage: UIImage, alpha: CGFloat = 1.0) -> UIImage {
@@ -443,9 +443,9 @@ extension ARSCNView {
 		let maxAngleInDeg = min(coneOpeningAngleInDegrees, 360) / 2
 		let maxAngle = ((maxAngleInDeg / 180) * Float.pi)
 		
-		let points = features.points
+		let points = features.__points
 		
-		for i in 0...features.count {
+		for i in 0...features.__count {
 			
 			let feature = points.advanced(by: Int(i))
 			let featurePos = SCNVector3(feature.pointee)
@@ -515,13 +515,13 @@ extension ARSCNView {
 			return nil
 		}
 		
-		let points = features.points
+		let points = features.__points
 		
 		// Determine the point from the whole point cloud which is closest to the hit test ray.
 		var closestFeaturePoint = origin
 		var minDistance = Float.greatestFiniteMagnitude
 		
-		for i in 0...features.count {
+		for i in 0...features.__count {
 			let feature = points.advanced(by: Int(i))
 			let featurePos = SCNVector3(feature.pointee)
 			
